9f559db0857284a8826a3940f3d540b7bf20b1e2
Lucas Laub <Lucas G. L.>
2024-03-01 21:18:56 +0100

    Added the code and README

---
+++ src/main.py
@@ -0,0 +1,21 @@
+# Std Libs
+
+# Third Party Libs
+from fastapi import FastAPI
+
+# Costume Libs
+
+
+def main() -> None:
+
+    # init routers
+
+    # init main router
+    App = FastAPI()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
